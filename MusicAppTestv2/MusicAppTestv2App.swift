a48998b4d0edeb849e8152941bfc780257a45b99
de4life <ageev_artur@ukr.net>
2022-10-17 22:04:54 +0300

    Initial Commit

---
+++ MusicAppTestv2/MusicAppTestv2App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppTestv2App.swift
+//  MusicAppTestv2
+//
+//  Created by Артур Агеев on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppTestv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
